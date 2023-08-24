c45aa2638c05368db11792969a691ce7eda6cf36
Gökay Şendil <gokaysendilw@outlook.com>
2023-08-24 21:39:33 +0300

    Hakkımızda kısmı tamamlandı responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./sections/Header/Header"
 import Hero from "./sections/Hero/Hero"
 import "./App.css"
 import Banner from "./sections/Banner/Banner"
+import About from "./sections/About/About"
 function App() {
   return (
     <div className="app">
       <Header/>
       <Hero/>
       <Banner/>
+      <About/>
       </div>
     
   )
---
+++ src/sections/About/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./about.css"
+import About1 from "../../assets/about1.png"
+import About2 from "../../assets/about2.png"
+const About = () => {
+  return (
+    <section className='container-fluid about-section'>
+      <h2 className='about-heading text-center'>NEDEN YARINLAR SİGORTA ?</h2>
+      <div className='container about-container custom-width'>
+      <div className='row w-100 d-flex  justify-content-center align-items-center'>
+        <div className='col-12 col-md-6 container about-text'>
+          <h2 className='about-sub'>Güvenilirlik ve Tecrübe</h2>
+          <p className='about-desc'>Farklı kategorilerdeki poliçelerle 
+          müşterilerimizin ihtiyaçlarına yönelik çözümler sunuyoruz. Ayrıca, 
+          özelleştirilebilir seçenekler ve farklı fiyat skalalarıyla müşterilerimizin 
+          bütçe ve tercihlerine uygun tekfliflerle, onların 
+          hayatlarını ve varlıklarını korumalarına destek oluyoruz</p>
+        </div>
+        <div className='col-12 col-md-6  container about-img-col'>
+          <img src={About1} alt='Neden Yarınlar Sigorta 1'/>
+        </div>
+      </div>
+      <div className='row w-100 d-flex  justify-content-center align-items-center'>
+      <div className='col-12 col-md-6  container about-img-col-2'>
+          <img src={About2} alt='Neden Yarınlar Sigorta 1'/>
+        </div>
+        <div className='col-12 col-md-6 container about-text'>
+          <h2 className='about-sub'>Geniş Ürün Yelpazesi</h2>
+          <p className='about-desc'>Hayat sigortası, sağlık sigortası, 
+          araç sigortası, seyahat sigortası, ev sigortası gibi farklı kategorilerdeki 
+          poliçelerle müşterilerimizin ihtiyaçlarına yönelik çözümler sunuyoruz.
+          Müşterilerimizin poliçeleriyle ilgili her türlü sorunlarında 
+          yanlarında olmak ve çözüm sunmak için en iyisini yapıyoruz.</p>
+        </div>
+        
+      </div>
+       </div>
+    </section>
+  )
+}
+
+export default About
